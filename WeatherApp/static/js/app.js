420a283df25bb92e129a837eecd44fc394855384
BDMC786 <44556616+BDMC786@users.noreply.github.com>
2019-12-05 15:28:29 -0500

    Update app.js

--- WeatherApp/static/js/app.js
+++ WeatherApp/static/js/app.js
@@ -1,15 +1,1 @@
-var input = d3.select("#scroll");
 
-input.on("click", function() {
-    d3.event.preventDefault();
-})
-
-// var element_to_scroll_to = document.getElementById('anchorName2');
-// // Or:
-// var element_to_scroll_to = document.querySelectorAll('.my-element-class')[0];
-// // Or:
-// var element_to_scroll_to = $('.my-element-class')[0];
-// // Basically `element_to_scroll_to` just have to be a reference
-// // to any DOM element present on the page
-// // Then:
-// element_to_scroll_to.scrollIntoView();id="filter-btn"
